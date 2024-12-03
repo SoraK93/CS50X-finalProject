0681d107c12d2e127310599b466d68a53783395e
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2024-12-03 06:45:07 +0530

    Create Table, Initialize DB

--- blog/__init__.py
+++ blog/__init__.py
@@ -7,9 +7,14 @@ class Base(DeclarativeBase):
     pass
 
 
+# Initial Config
 db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.__init__(app)
+
+from blog import models
+with app.app_context():
+    db.create_all()
 
 from blog import routes
---
+++ blog/models.py
@@ -0,0 +1,49 @@
+from blog import db
+from datetime import datetime
+from sqlalchemy import Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+
+
+# This table is for storing Users information
+class User(db.Model):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    profile_photo: Mapped[str] = mapped_column(String(20), nullable=False, default="default.jpg")
+    # Below line will link user table to post table
+    user_post = relationship("Post", back_populates="author")
+    # Below line will link user table to comment table
+    user_comment = relationship("Comment", back_populates="user")
+
+
+# This table stores Posts created by Users.
+# Post table has a many(post)-to-one(user) relationship with User table
+# Post table has a many(comment)-to-one(post) relationship with Comment table
+class Post(db.Model):
+    __tablename__ = "post"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    datetime: Mapped[str] = mapped_column(DateTime, nullable=False, default=datetime.now)
+    # Below lines will link post table to user table
+    author_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+    author = relationship("User", back_populates="user_post")
+    # Below lines will link post table to comment table
+    post_comment = relationship("Comment", back_populates="feedback")
+
+
+# This table stores Users comment made on any individual post
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    # These lines will link user to comment table in a one-to-many relationship
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+    user = relationship("User", back_populates="user_comment")
+    # These lines will link post to comment table in a one-to-many relationship
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey("post.id"), nullable=False)
+    feedback = relationship("Post", back_populates="post_comment")
+
